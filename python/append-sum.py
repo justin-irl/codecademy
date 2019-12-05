9453af6c5626996d4cca74c81d4cc64ce25660ee
Justin <replicamask@gmail.com>
2019-12-05 00:37:40 -0500

    finished round of lessons

--- python/append-sum.py
+++ python/append-sum.py
@@ -3,3 +3,14 @@
 #The function should add the last two elements of lst together and append the result to lst. It should do this process three times and then return lst.
 
 #For example, if lst started as [1, 1, 2], the final result should be [1, 1, 2, 3, 5, 8].
+
+def append_sum(lst):
+    for each in range (3):
+        attach = lst[-1] + lst[-2]
+        lst.append(attach)
+    return lst
+
+
+print(append_sum([1, 1, 2]))
+
+# solved correctly!
